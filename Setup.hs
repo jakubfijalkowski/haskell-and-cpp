bb74262e677021a2489b8be910e2f4699eebd690
Jakub Fijałkowski <fiolek94@gmail.com>
2015-04-17 00:46:21 +0200

    Build external library on configure.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,16 @@
-import Distribution.Simple
-main = defaultMain
+import           Distribution.PackageDescription hiding (Flag)
+import           Distribution.Simple
+import           Distribution.Simple.Setup
+import           Distribution.Simple.Utils
+
+main = defaultMainWithHooks simpleUserHooks
+  {
+    preConf = makeExtLib
+  }
+
+makeExtLib :: Args -> ConfigFlags -> IO HookedBuildInfo
+makeExtLib _ flags = do
+    let verbosity = fromFlag $ configVerbosity flags
+    rawSystemExit verbosity "env"
+        ["make", "--directory=ext_lib"]
+    return emptyHookedBuildInfo
