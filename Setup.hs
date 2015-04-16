395b8388d977ab0c471b24938fd31c0dd9a853ad
Jakub Fijałkowski <fiolek94@gmail.com>
2015-04-17 00:51:33 +0200

    Update extra-lib-dirs from Setup.hs.

--- Setup.hs
+++ Setup.hs
@@ -1,11 +1,15 @@
-import           Distribution.PackageDescription hiding (Flag)
+import           Data.Maybe
+import           Distribution.PackageDescription    hiding (Flag)
 import           Distribution.Simple
+import           Distribution.Simple.LocalBuildInfo
 import           Distribution.Simple.Setup
 import           Distribution.Simple.Utils
+import           System.Directory
 
 main = defaultMainWithHooks simpleUserHooks
   {
     preConf = makeExtLib
+  , confHook = \a f -> confHook simpleUserHooks a f >>= updateExtraLibDirs
   }
 
 makeExtLib :: Args -> ConfigFlags -> IO HookedBuildInfo
@@ -14,3 +18,20 @@ makeExtLib _ flags = do
     rawSystemExit verbosity "env"
         ["make", "--directory=ext_lib"]
     return emptyHookedBuildInfo
+
+updateExtraLibDirs :: LocalBuildInfo -> IO LocalBuildInfo
+updateExtraLibDirs localBuildInfo = do
+    let packageDescription = localPkgDescr localBuildInfo
+        lib = fromJust $ library packageDescription
+        libBuild = libBuildInfo lib
+    dir <- getCurrentDirectory
+    return localBuildInfo {
+        localPkgDescr = packageDescription {
+            library = Just $ lib {
+                libBuildInfo = libBuild {
+                    extraLibDirs = (dir ++ "/ext_lib/lib") :
+                        extraLibDirs libBuild
+                }
+            }
+        }
+    }
