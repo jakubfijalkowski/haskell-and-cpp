7257400db8b6c5a43f8f8f6fd7dbbac43ede5138
Jakub Fijałkowski <fiolek94@gmail.com>
2015-04-17 01:00:26 +0200

    Add FFI bindings to c_randomNumber.

--- src/Sample.hs
+++ src/Sample.hs
@@ -1,1 +1,5 @@
 module Sample where
+
+import Foreign.C
+
+foreign import ccall "capi.h" c_randomNumber :: IO CInt
--- test/Main.hs
+++ test/Main.hs
@@ -5,4 +5,4 @@ main :: IO ()
 main = hspec $ do
     describe "sample_function" $ do
         it "should return 785" $ do
-            pendingWith "add bindings"
+            c_randomNumber `shouldReturn` 785
